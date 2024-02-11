6a19200732ffaea0c4ff8524c64ba7fc33e00813
Prajwal Prasad <prajwalpr11@gmail.com>
2024-02-10 23:18:47 -0500

    added thanks

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function beforesubmit(){
      let inputdate = document.querySelector(".inputdate");
      console.log(outputdate.value, inputdate.value);
 
-     let formatteddate = new Date(inputdate.value).toLocaleDateString("en-US");
+     let formatteddate = new Date(inputdate.value ) .toLocaleDateString("en-US");
      outputdate.value = formatteddate;
 }
