204c193f183082f472f202c681b6d0f6ada28b67
Prajwal Prasad <prajwalpr11@gmail.com>
2024-02-10 23:10:09 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function beforesubmit(){
+     let outputdate = document.querySelector(".outputdate");
+     let inputdate = document.querySelector(".inputdate");
+     console.log(outputdate.value, inputdate.value);
+
+     let formatteddate = new Date(inputdate.value).toLocaleDateString("en-US");
+     outputdate.value = formatteddate;
+}
